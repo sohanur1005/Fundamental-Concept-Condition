d80632209539efefc15277ca333de54a555de670
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 23:39:44 +0600

    send this

---
+++ condition.js
@@ -0,0 +1,29 @@
+var salary=25000;
+var height=60;
+var isBCS=true;
+if(salary>20000)
+{
+    console.log('Biya Done')
+}
+else{
+    console.log('Goribs')
+}
+
+
+if(salary>20000 && height>72)
+{
+    console.log('bolo baba kobul')
+}
+else{
+    console.log('Biye Cancell')
+}
+
+
+if((salary>20000 && height>72) || isBCS==true)
+{
+    console.log('14 gusti raji ')
+}
+else{
+    console.log('Ja vag')
+}
+
