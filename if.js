20f9abaf620b49c0e7978702129432e95dedb42d
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 19:16:55 +0600

    send this

---
+++ if.js
@@ -0,0 +1,18 @@
+if(5>10)
+{
+    console.log('Greater Number')
+}
+else{
+    console.log('Smaller Number')
+}
+
+
+var weight=40;
+
+if(weight < 20)
+{
+    console.log('I will carry')
+}
+else{
+    console.log('I will rent a  rickshaw')
+}
