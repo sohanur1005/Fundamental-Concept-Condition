4bad573029c273f586851f80766e90c52e387a17
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 18:08:07 +0600

    send this

--- comparison.js
+++ comparison.js
@@ -1,2 +1,8 @@
-console.log(5>10)
-console.log(5<10)
+// console.log(5>10)
+// console.log(5<10)
+// console.log(5==10)
+// console.log(5<=10)
+
+var peyaraPrice=50;
+var applePrice=100;
+console.log(peyaraPrice < applePrice)
