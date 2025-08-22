4a44e31a70b8544aeb00b85bcde53b8044483207
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-22 23:49:52 +0600

    send this

---
+++ multi-if-else.js
@@ -0,0 +1,17 @@
+var price=5000;
+if(price>5000)
+{
+    // 10%
+    var discount=price * 10/100;
+    var payAmount=price-discount;
+    console.log(payAmount)
+}
+else if(price > 3000)
+{
+    var discount=price * 5/100;
+    var payAmount=price-discount;
+    console.log(payAmount)
+}
+else{
+    console.log(price)
+}
