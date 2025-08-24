4ab371648122f093a8abb2f414f6d66b7b1be246
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-24 19:50:56 +0600

    send this

---
+++ nested.js
@@ -0,0 +1,21 @@
+const money=300;
+if(money > 500)
+{
+    console.log('bro Your are rich')
+}
+else {
+    if(money > 100)
+    {
+        console.log("tui gorib o na borolok o na")
+    }
+
+    else{
+        if(money > 0)
+        {
+            console.log('Kola kha')
+        }
+        else{
+            console.log('sala fokir')
+        }
+    }
+}
